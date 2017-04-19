896e350a84770ea3f00b967d706b02d1b83c113d
David Kebler <d@kebler.net>
2017-04-19 16:57:54 -0700

    Updates

--- js/page.js
+++ js/page.js
@@ -1,17 +1,17 @@
 // Smooth Scroll Init - Register click handler for ID anchors
 $('a[href*="#"]:not(a[modal])').click(function () {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      var targetOffset = target.offset().top - $(".nav-bar__header").outerHeight(true);
-      // console.log(targetOffset, target.offset().top, $(".nav-bar__header").outerHeight())
-      $('html, body').animate({
-        scrollTop: targetOffset
-      }, 1000);
-      return false;
-    }
+  // if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  var target = $(this.hash);
+  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+  if (target.length) {
+    var targetOffset = target.offset().top - $(".nav-bar__header").outerHeight(true);
+    // console.log(targetOffset, target.offset().top, $(".nav-bar__header").outerHeight())
+    $('html, body').animate({
+      scrollTop: targetOffset
+    }, 1000);
+    return false;
   }
+  // }
 });
 
 // navbar mobile toggle - preload
