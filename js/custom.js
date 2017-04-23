72257cff9d1ea3885665ba56673f40220634674c
David Kebler <d@kebler.net>
2017-04-22 20:14:34 -0700

    Updates

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,4 @@
-function nothing() {
-  return false;
+if (!(window.location.host.startsWith("127.0.0.1") || window.location.host.startsWith("localhost")) && (window.location.protocol != "https:")) {
+  window.location.protocol = "https";
+  document.location = "https:" + window.location.href.substring(window.location.protocol.length, window.location.href.length);
 }
