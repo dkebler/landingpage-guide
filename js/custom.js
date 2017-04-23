facac6bc362d3d5c50c580ee9ccd608898a49f1b
David Kebler <d@kebler.net>
2017-04-22 20:23:21 -0700

    Updates

--- js/custom.js
+++
@@ -1,4 +0,0 @@
-if (!(window.location.host.startsWith("127.0.0.1") || window.location.host.startsWith("localhost")) && (window.location.protocol != "https:")) {
-  window.location.protocol = "https";
-  document.location = "https:" + window.location.href.substring(window.location.protocol.length, window.location.href.length);
-}
